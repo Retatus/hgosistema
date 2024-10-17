296c0ca638bd068c941630ce7010a9e6d54afb9d
rguevarac85 <rguevarac85@hotmail.com>
2024-10-17 17:18:52 -0500

    correccion de campo treencauchadora

--- app/Models/AuditoriaModel.php
+++ app/Models/AuditoriaModel.php
@@ -41,7 +41,7 @@ class AuditoriaModel extends Model
 
 		$builder = $this->conexion('tauditoria t0');
 
-		$builder->select("t0.nidauditoria idauditoria, t0.scampo_modificado campo_modificado, t0.svalor_anterior valor_anterior, t0.svalor_nuevo valor_nuevo, t0.tfecha_modificacion fecha_modificacion, t0.susuario_modificacion usuario_modificacion, t0.bestado estado, t1.nidservicio idservicio, t2.sidcliente idcliente, t2.snombrecliente nombrecliente, t3.nidubicacion idubicacion, t3.snombretipoubicacion nombretipoubicacion, t4.nidbanda idbanda, t4.snombrebanda nombrebanda, t5.nidcondicion idcondicion, t5.snombrecondicion nombrecondicion, t6.nidneumatico idneumatico, t6.snombreneumatico nombreneumatico, t7.nidrencauchadora idrencauchadora, t7.snombrereencauchadora nombrereencauchadora, t8.nidtiposervicio idtiposervicio, t8.snombretiposervicio nombretiposervicio, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenado, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenadodetalle");
+		$builder->select("t0.nidauditoria idauditoria, t0.scampo_modificado campo_modificado, t0.svalor_anterior valor_anterior, t0.svalor_nuevo valor_nuevo, t0.tfecha_modificacion fecha_modificacion, t0.susuario_modificacion usuario_modificacion, t0.bestado estado, t1.nidservicio idservicio, t2.sidcliente idcliente, t2.snombrecliente nombrecliente, t3.nidubicacion idubicacion, t3.snombretipoubicacion nombretipoubicacion, t4.nidbanda idbanda, t4.snombrebanda nombrebanda, t5.nidcondicion idcondicion, t5.snombrecondicion nombrecondicion, t6.nidneumatico idneumatico, t6.snombreneumatico nombreneumatico, t7.nidreencauchadora idrencauchadora, t7.snombrereencauchadora nombrereencauchadora, t8.nidtiposervicio idtiposervicio, t8.snombretiposervicio nombretiposervicio, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenado, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenadodetalle");
 
 		$builder->join('tservicio t1', 't1.nidservicio = t0.nidservicio');
 		$builder->join('tcliente t2', 't2.sidcliente = t1.sidcliente');
@@ -49,7 +49,7 @@ class AuditoriaModel extends Model
 		$builder->join('tbanda t4', 't4.nidbanda = t1.nidbanda');
 		$builder->join('tcondicion t5', 't5.nidcondicion = t1.nidcondicion');
 		$builder->join('tneumatico t6', 't6.nidneumatico = t1.nidneumatico');
-		$builder->join('treencauchadora t7', 't7.nidrencauchadora = t1.nidrencauchadora');
+		$builder->join('treencauchadora t7', 't7.nidreencauchadora = t1.nidreencauchadora');
 		$builder->join('ttiposervicio t8', 't8.nidtiposervicio = t1.nidtiposervicio');
 
 		if ($todos !== '') {
@@ -80,14 +80,14 @@ class AuditoriaModel extends Model
 	public function getAutocompleteAuditorias($todos = 1, $text = ''){
 		$builder = $this->conexion('tauditoria t0');
 
-		$builder->select("t0.nidauditoria idauditoria, t0.scampo_modificado campo_modificado, t0.svalor_anterior valor_anterior, t0.svalor_nuevo valor_nuevo, t0.tfecha_modificacion fecha_modificacion, t0.susuario_modificacion usuario_modificacion, t0.bestado estado, t1.nidservicio idservicio, t2.sidcliente idcliente, t2.snombrecliente nombrecliente, t3.nidubicacion idubicacion, t3.snombretipoubicacion nombretipoubicacion, t4.nidbanda idbanda, t4.snombrebanda nombrebanda, t5.nidcondicion idcondicion, t5.snombrecondicion nombrecondicion, t6.nidneumatico idneumatico, t6.snombreneumatico nombreneumatico, t7.nidrencauchadora idrencauchadora, t7.snombrereencauchadora nombrereencauchadora, t8.nidtiposervicio idtiposervicio, t8.snombretiposervicio nombretiposervicio, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenado, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenadodetalle");
+		$builder->select("t0.nidauditoria idauditoria, t0.scampo_modificado campo_modificado, t0.svalor_anterior valor_anterior, t0.svalor_nuevo valor_nuevo, t0.tfecha_modificacion fecha_modificacion, t0.susuario_modificacion usuario_modificacion, t0.bestado estado, t1.nidservicio idservicio, t2.sidcliente idcliente, t2.snombrecliente nombrecliente, t3.nidubicacion idubicacion, t3.snombretipoubicacion nombretipoubicacion, t4.nidbanda idbanda, t4.snombrebanda nombrebanda, t5.nidcondicion idcondicion, t5.snombrecondicion nombrecondicion, t6.nidneumatico idneumatico, t6.snombreneumatico nombreneumatico, t7.nidreencauchadora idrencauchadora, t7.snombrereencauchadora nombrereencauchadora, t8.nidtiposervicio idtiposervicio, t8.snombretiposervicio nombretiposervicio, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenado, CONCAT(t2.snombrecliente,' - ',t3.snombretipoubicacion,' - ',t4.snombrebanda,' - ',t5.snombrecondicion,' - ',t6.snombreneumatico,' - ',t7.snombrereencauchadora,' - ',t8.snombretiposervicio) concatenadodetalle");
 		$builder->join('tservicio t1', 't1.nidservicio = t0.nidservicio');
 		$builder->join('tcliente t2', 't2.sidcliente = t1.sidcliente');
 		$builder->join('tubicacion t3', 't3.nidubicacion = t1.nidubicacion');
 		$builder->join('tbanda t4', 't4.nidbanda = t1.nidbanda');
 		$builder->join('tcondicion t5', 't5.nidcondicion = t1.nidcondicion');
 		$builder->join('tneumatico t6', 't6.nidneumatico = t1.nidneumatico');
-		$builder->join('treencauchadora t7', 't7.nidrencauchadora = t1.nidrencauchadora');
+		$builder->join('treencauchadora t7', 't7.nidreencauchadora = t1.nidreencauchadora');
 		$builder->join('ttiposervicio t8', 't8.nidtiposervicio = t1.nidtiposervicio');
 
 		if ($todos !== '') {
@@ -141,7 +141,7 @@ class AuditoriaModel extends Model
 		$builder->join('tbanda t4', 't4.nidbanda = t1.nidbanda');
 		$builder->join('tcondicion t5', 't5.nidcondicion = t1.nidcondicion');
 		$builder->join('tneumatico t6', 't6.nidneumatico = t1.nidneumatico');
-		$builder->join('treencauchadora t7', 't7.nidrencauchadora = t1.nidrencauchadora');
+		$builder->join('treencauchadora t7', 't7.nidreencauchadora = t1.nidreencauchadora');
 		$builder->join('ttiposervicio t8', 't8.nidtiposervicio = t1.nidtiposervicio');
 		$builder->where('t0.nidauditoria', $id);
 		$query = $builder->get();
@@ -157,7 +157,7 @@ class AuditoriaModel extends Model
 		$builder->join('tbanda t4', 't4.nidbanda = t1.nidbanda');
 		$builder->join('tcondicion t5', 't5.nidcondicion = t1.nidcondicion');
 		$builder->join('tneumatico t6', 't6.nidneumatico = t1.nidneumatico');
-		$builder->join('treencauchadora t7', 't7.nidrencauchadora = t1.nidrencauchadora');
+		$builder->join('treencauchadora t7', 't7.nidreencauchadora = t1.nidreencauchadora');
 		$builder->join('ttiposervicio t8', 't8.nidtiposervicio = t1.nidtiposervicio');
 
 		if ($todos !== '') {
